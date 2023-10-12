6c42d0fdda30e8a903ec14c5bc7ab6b18695387c
W0825064 <W0825064@ADL-A1201I-25>
2023-10-12 17:39:30 -0400

    Add project files.

---
+++ Controllers/PenCompanyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace PenCompany.Controllers
+
+{
+    public class PenCompanyController : Controller
+
+    {
+        // 
+        // GET: /PenCompany
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /PenCompany/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
